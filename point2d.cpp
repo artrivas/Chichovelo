ea3b9c8755b56bb92a4771feaab8f8f50697b872
juaquin456 <juaquin.remon@utec.edu.pe>
2024-09-17 17:15:20 -0500

    Add geometry templates

---
+++ point2d.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<long long> vl;
+typedef pair<long long,long long> pl;
+typedef vector<pair<long long,long long>> vpl;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll mod = 1e9+7;
+const ll inf = 1e18+10;
+
+typedef ll ftype;
+struct point2d {
+    ftype x, y;
+    point2d() {}
+    point2d(ftype x, ftype y): x(x), y(y) {}
+    point2d& operator+=(const point2d &t) {
+        x += t.x;
+        y += t.y;
+        return *this;
+    }
+    point2d& operator-=(const point2d &t) {
+        x -= t.x;
+        y -= t.y;
+        return *this;
+    }
+    point2d& operator*=(ftype t) {
+        x *= t;
+        y *= t;
+        return *this;
+    }
+    point2d& operator/=(ftype t) {
+        x /= t;
+        y /= t;
+        return *this;
+    }
+    point2d operator+(const point2d &t) const {
+        return point2d(*this) += t;
+    }
+    point2d operator-(const point2d &t) const {
+        return point2d(*this) -= t;
+    }
+    point2d operator*(ftype t) const {
+        return point2d(*this) *= t;
+    }
+    point2d operator/(ftype t) const {
+        return point2d(*this) /= t;
+    }
+};
+
+point2d operator*(ftype a, point2d b) {
+    return b * a;
+}
+ftype dot(point2d a, point2d b) {
+    return a.x * b.x + a.y * b.y;
+}
+ftype norm(point2d a) {
+    return dot(a, a);
+}
+double abs(point2d a) {
+    return sqrt(norm(a));
+}
+ftype cross(point2d a, point2d b) {
+    return a.x * b.y - a.y * b.x;
+}
---
+++ point3d.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<long long> vl;
+typedef pair<long long,long long> pl;
+typedef vector<pair<long long,long long>> vpl;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll mod = 1e9+7;
+const ll inf = 1e18+10;
+
+typedef ll ftype;
+
+struct point3d {
+    ftype x, y, z;
+    point3d() {}
+    point3d(ftype x, ftype y, ftype z): x(x), y(y), z(z) {}
+    point3d& operator+=(const point3d &t) {
+        x += t.x;
+        y += t.y;
+        z += t.z;
+        return *this;
+    }
+    point3d& operator-=(const point3d &t) {
+        x -= t.x;
+        y -= t.y;
+        z -= t.z;
+        return *this;
+    }
+    point3d& operator*=(ftype t) {
+        x *= t;
+        y *= t;
+        z *= t;
+        return *this;
+    }
+    point3d& operator/=(ftype t) {
+        x /= t;
+        y /= t;
+        z /= t;
+        return *this;
+    }
+    point3d operator+(const point3d &t) const {
+        return point3d(*this) += t;
+    }
+    point3d operator-(const point3d &t) const {
+        return point3d(*this) -= t;
+    }
+    point3d operator*(ftype t) const {
+        return point3d(*this) *= t;
+    }
+    point3d operator/(ftype t) const {
+        return point3d(*this) /= t;
+    }
+};
+point3d operator*(ftype a, point3d b) {
+    return b * a;
+}
+ftype dot(point3d a, point3d b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+point3d cross(point3d a, point3d b) {
+    return point3d(a.y * b.z - a.z * b.y,
+                   a.z * b.x - a.x * b.z,
+                   a.x * b.y - a.y * b.x);
+}
