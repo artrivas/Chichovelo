6f4b13a8de2fdcf366dcb2b86259f1a1d20a2dd3
juaquin456 <juaquin.remon@utec.edu.pe>
2024-10-09 12:29:17 -0500

    add template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<long long> vl;
+typedef pair<long long,long long> pl;
+typedef vector<pair<long long,long long>> vpl;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll mod = 1e9+7;
+const ll inf = 1e18+10;
+
+void solve(){
+	
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int tt = 1;
+    cin >> tt;
+    while(tt--) solve();
+    return 0;
+}
+
