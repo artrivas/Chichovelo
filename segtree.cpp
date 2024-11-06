88a1914e411ccbc296484c5e756c293ae9177981
juaquin456 <juaquin.remon@utec.edu.pe>
2024-11-06 18:05:29 -0500

    update segtrees

--- segtree.cpp
+++ segtree.cpp
@@ -16,68 +16,25 @@ typedef vector<pair<long long,long long>> vpl;
 const ll mod = 1e9+7;
 const ll inf = 1e18+10;
 
-struct node {
-  ll sum;
-};
-
-node combine(const node& l, const node& r) {
-  node ans;
-  ans.sum = l.sum + r.sum;
-  return ans;
-}
-
-node make_node(ll val) {
-  if (val == inf) {
-    return {};
-  }
-  return {val};
-}
-
-class st {
-  vector<node> t;
-  ll n;
-  void build(const vl&a, ll v, ll tl, ll tr) {
-    if (tl == tr) {
-      t[v] = make_node(a[tl]);
-    }
-    else {
-      ll tm = (tl + tr) / 2;
-      build(a, v*2, tl, tm);
-      build(a, v*2+1, tm+1, tr);
-      t[v] = combine(t[v*2], t[v*2+1]);
-    }
-  }
-
-  void update(ll v, ll tl, ll tr, ll pos, ll new_val) {
-    if (tl == tr) {
-      t[v] = make_node(new_val);
-    }
-    else {
-      ll tm = (tl + tr) / 2;
-      if (pos <= tm) update(v*2, tl, tm, pos, new_val);
-      else update(v*2+1, tm+1, tr, pos, new_val);
-      t[v] = combine(t[v*2], t[v*2+1]);
-    }
-  }
-
-  node query(const ll& v, const ll& tl, const ll& tr, const ll& l, const ll& r) {
-    if (l > r) return make_node(inf);
-    if (l == tl && r == tr) return t[v];
-    ll tm = (tl + tr)/2;
-    return combine(query(v*2, tl, tm, l, min(r, tm)), query(v*2+1, tm+1, tr, max(l, tm+1), r));
-  }
- public:
-  st(const vl& a) {
-    this->n = a.size();
-    t.resize(4*n);
-    build(a, 1, 0, n-1);
-  }
-
-  void update(ll pos, ll new_val) {
-    update(1, 0, n-1, pos, new_val);
-  }
-
-  node query(ll l, ll r) {
-    return query(1, 0, n-1, l, r);
-  }
+struct Mono { /* TODO */ static Mono zero() { /* TODO */ } };
+Mono operator+ (Mono a, Mono b) { /* TODO */ }
+struct Segtree {
+	int len; vector<Mono> data;
+	void init(Mono* a, int n) { assert(n>0); // O(n+len)
+		len=1<<(31-__builtin_clz(2*n-1));
+		data.assign(len*2, Mono::zero());
+		if (a) for (int i = 0; i < n; i++) data[i+len] = a[i];
+		for (int i = len - 1; i >= 1; i--) data[i] = data[i*2] + data[i*2+1];
+	}
+	void update(int i, Mono x) { i += len; data[i] = x;
+		while (i /= 2) data[i] = data[i*2] + data[i*2+1];
+	}
+	Mono query(int l, int r) { ql = l; qr = r; return q_(1, 0, len); }
+	int ql, qr;
+	Mono q_(int i, int l, int r) {
+		if (r <= ql || qr <= l) return Mono::zero();
+		if (ql <= l && r <= qr) return data[i];
+		int m = (l+r)/2;
+		return q_(i*2,l,m) + q_(i*2+1,m,r);
+	}
 };
---
+++ segtree_lazy.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll mod = 1e9+7;
+const ll inf = 1e18+10;
+
+struct Lazy { /* TODO */
+	static Lazy zero() { /* TODO */ }
+	bool is_zero() { /* TODO */ } };
+
+struct Mono { /* TODO */
+	static Mono zero() { /* TODO */ }
+	void apply_lazy(Lazy d, int l, int r) { /* TODO */ } };
+
+Lazy operator+ (Lazy a, Lazy b) { /* TODO */ }
+Mono operator+ (Mono a, Mono b) { /* TODO */ }
+
+struct SegtreeLazy {
+	int len; vector<Mono> data; vector<Lazy> lazy;
+	void init(Mono* a, int n) { assert(n>0); // O(n+len)
+		len=1<<(31-__builtin_clz(2*n-1));
+		data.assign(2*len, Mono::zero());
+		lazy.assign(2*len, Lazy::zero());
+		if (a) for (int i = 0; i < n; i++) data[i+len] = a[i];
+		for (int i = len - 1; i >= 1; i--) data[i] = data[i*2] + data[i*2+1];
+	}
+	void update(int l, int r, Lazy x) { ql = l; qr = r; u_(1, 0, len, x); }
+	Mono query(int l, int r) { ql = l; qr = r; return q_(1, 0, len); }
+	int ql, qr;
+	void p_(int i, int l, int r) {
+		if (lazy[i].is_zero()) return;
+		if (i < len) {
+			lazy[2*i] = lazy[2*i] + lazy[i];
+			lazy[2*i+1] = lazy[2*i+1] + lazy[i];
+		}
+		data[i].apply_lazy(lazy[i], l, r);
+		lazy[i] = Lazy::zero();
+	}
+	Mono q_(int i, int l, int r) {
+		if (r <= ql || qr <= l) return Mono::zero();
+		p_(i,l,r);
+		if (ql <= l && r <= qr) return data[i];
+		int m = (l+r)/2;
+		return q_(i*2,l,m) + q_(i*2+1,m,r);
+	}
+	void u_(int i, int l, int r, Lazy x) {
+		if (l == r) return;
+		p_(i,l,r);
+		if (r <= ql || qr <= l) return;
+		if (ql <= l && r <= qr) {
+			lazy[i] = lazy[i] + x;
+			p_(i,l,r);
+			return;
+		}
+		int m = (l+r)/2;
+		u_(i*2,l,m,x); u_(i*2+1,m,r,x);
+		data[i] = data[2*i] + data[2*i+1];
+	}
+};
