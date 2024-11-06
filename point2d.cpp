ec476d3e9f982a2f6ac24fcf6f6e2929c12ca032
juaquin456 <juaquin.remon@utec.edu.pe>
2024-11-05 23:12:28 -0500

    update point2d

--- point2d.cpp
+++ point2d.cpp
@@ -18,57 +18,76 @@ const ll inf = 1e18+10;
 
 typedef ll ftype;
 struct point2d {
-    ftype x, y;
-    point2d() {}
-    point2d(ftype x, ftype y): x(x), y(y) {}
-    point2d& operator+=(const point2d &t) {
-        x += t.x;
-        y += t.y;
-        return *this;
-    }
-    point2d& operator-=(const point2d &t) {
-        x -= t.x;
-        y -= t.y;
-        return *this;
-    }
-    point2d& operator*=(ftype t) {
-        x *= t;
-        y *= t;
-        return *this;
-    }
-    point2d& operator/=(ftype t) {
-        x /= t;
-        y /= t;
-        return *this;
-    }
-    point2d operator+(const point2d &t) const {
-        return point2d(*this) += t;
-    }
-    point2d operator-(const point2d &t) const {
-        return point2d(*this) -= t;
-    }
-    point2d operator*(ftype t) const {
-        return point2d(*this) *= t;
-    }
-    point2d operator/(ftype t) const {
-        return point2d(*this) /= t;
-    }
+  ftype x, y;
+  point2d() {}
+  point2d(ftype x, ftype y): x(x), y(y) {}
+  point2d& operator+=(const point2d &t) {
+    x += t.x;
+    y += t.y;
+    return *this;
+  }
+  point2d& operator-=(const point2d &t) {
+    x -= t.x;
+    y -= t.y;
+    return *this;
+  }
+  point2d& operator*=(ftype t) {
+    x *= t;
+    y *= t;
+    return *this;
+  }
+  point2d& operator/=(ftype t) {
+    x /= t;
+    y /= t;
+    return *this;
+  }
+  point2d operator+(const point2d &t) const {
+    return point2d(*this) += t;
+  }
+  point2d operator-(const point2d &t) const {
+    return point2d(*this) -= t;
+  }
+  point2d operator*(ftype t) const {
+    return point2d(*this) *= t;
+  }
+  point2d operator/(ftype t) const {
+    return point2d(*this) /= t;
+  }
 };
 
 point2d operator*(ftype a, point2d b) {
-    return b * a;
+  return b * a;
 }
 ftype dot(point2d a, point2d b) {
-    return a.x * b.x + a.y * b.y;
+  return a.x * b.x + a.y * b.y;
 }
 ftype norm(point2d a) {
-    return dot(a, a);
+  return dot(a, a);
 }
 double abs(point2d a) {
-    return sqrt(norm(a));
+  return sqrt(norm(a));
 }
 ftype cross(point2d a, point2d b) {
-    return a.x * b.y - a.y * b.x;
+  return a.x * b.y - a.y * b.x;
+}
+
+int sgn(const long long& x) { return x >= 0 ? x ? 1 : 0 : -1; }
+
+// intersection of 2 segements 1d
+bool inter1(long long a, long long b, long long c, long long d) {
+  if (a > b)
+    swap(a, b);
+  if (c > d)
+    swap(c, d);
+  return max(a, c) <= min(b, d);
+}
+
+// intersection of 2 segments ab cd
+bool check_inter(const point2d& a, const point2d& b, const point2d& c, const point2d& d) {
+  if (cross(a - c, d - c) == 0 && cross(b - c, d - c) == 0)
+    return inter1(a.x, b.x, c.x, d.x) && inter1(a.y, b.y, c.y, d.y);
+  return sgn(cross(b - a, c - a)) != sgn(cross(b - a, d - a)) &&
+       sgn(cross(d - c, a - c)) != sgn(cross(d - c, b - c));
 }
 
 // atan2(y, x) -> transform cartesiano to angle [-pi: pi]
